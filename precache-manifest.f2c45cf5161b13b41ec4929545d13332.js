4c1d87e10db2de9611f4ec9a967702e0a315f5cb
Bayramgeldi <bnurberdiyev@colotek.com>
2020-11-04 21:03:57 -0500

    Updates

---
+++ precache-manifest.f2c45cf5161b13b41ec4929545d13332.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6df4451d17b0b468bf14a45af9f76902",
+    "url": "/vidly-app/index.html"
+  },
+  {
+    "revision": "29ad1144e1d842f5e870",
+    "url": "/vidly-app/static/css/2.ba3dfce7.chunk.css"
+  },
+  {
+    "revision": "72baf95f6b97d889a4ed",
+    "url": "/vidly-app/static/css/main.7385f87e.chunk.css"
+  },
+  {
+    "revision": "29ad1144e1d842f5e870",
+    "url": "/vidly-app/static/js/2.5d34f29a.chunk.js"
+  },
+  {
+    "revision": "494f8d1560152b6d529f5ed806252e77",
+    "url": "/vidly-app/static/js/2.5d34f29a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72baf95f6b97d889a4ed",
+    "url": "/vidly-app/static/js/main.e63932f8.chunk.js"
+  },
+  {
+    "revision": "e36886ce1a831ae8e0af",
+    "url": "/vidly-app/static/js/runtime-main.348b7271.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/vidly-app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/vidly-app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/vidly-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/vidly-app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/vidly-app/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
